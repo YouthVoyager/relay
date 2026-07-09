ef54e4e4dd9b3bf075ff6c46a2aa1461dcf30394
YouthVoyager <2893666874@qq.com>
2026-07-09 21:17:29 +0800

    项目初始化

---
+++ cmd/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"ok"}`))
+	})
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Println("relay server listening on :8080")
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/YouthVoyager/relay
+
+go 1.26.5
